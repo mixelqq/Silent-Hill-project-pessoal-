98499d6416b6def1114b6bfe891aa2a20f889402
mox <miguel.fleck@rede.ulbra.br>
2026-05-06 21:36:50 -0300

    aa

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function toggleMenu() {
     menu.classList.toggle("ativo");
 
 }
+
+function login() {
+
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+
+    if(email === "admin@gmail.com" && senha === "1234") {
+
+        localStorage.setItem("logado", "true");
+        localStorage.setItem("usuario", email);
+
+        window.location.href = "index.html";
+
+    } else {
+
+        alert("Login inválido");
+
+    }
+}
