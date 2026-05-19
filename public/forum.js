f0bf76f7bbe662327b12f8e98865ac9aed4d6532
mox <miguel.fleck@rede.ulbra.br>
2026-05-19 20:00:51 -0300

    aa

--- public/forum.js
+++ public/forum.js
@@ -65,3 +65,71 @@ function medoPost(id){
     animarBotao("medo-" + id);
 
 }
+
+const postsDiv = document.getElementById("posts");
+
+let posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+renderizarPosts();
+
+function criarPost() {
+
+    const titulo = document.getElementById("titulo").value;
+
+    const mensagem = document.getElementById("mensagem").value;
+
+    if (titulo === "" || mensagem === "") {
+
+        alert("Preencha tudo!");
+
+        return;
+    }
+
+    const novoPost = {
+
+        titulo,
+        mensagem
+    };
+
+    posts.push(novoPost);
+
+    localStorage.setItem("posts", JSON.stringify(posts));
+
+    renderizarPosts();
+
+    document.getElementById("titulo").value = "";
+
+    document.getElementById("mensagem").value = "";
+}
+
+function renderizarPosts() {
+
+    postsDiv.innerHTML = "";
+
+    posts.forEach((post, index) => {
+
+        postsDiv.innerHTML += `
+
+        <div class="post">
+
+            <h2>${post.titulo}</h2>
+
+            <p>${post.mensagem}</p>
+
+            <button onclick="deletarPost(${index})">
+            Excluir
+            </button>
+
+        </div>
+        `;
+    });
+}
+
+function deletarPost(index) {
+
+    posts.splice(index, 1);
+
+    localStorage.setItem("posts", JSON.stringify(posts));
+
+    renderizarPosts();
+}
