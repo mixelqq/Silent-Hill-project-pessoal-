05925d1aced8929284bfd82a2b0c2a57e1ea65e1
mox <miguel.fleck@rede.ulbra.br>
2026-06-25 14:07:46 -0300

    Update silenthill.session.sql

---
+++ silenthill.session.sql
@@ -0,0 +1,25 @@
+INSERT INTO posts (id, titulo, mensagem, autor, curtidas, fixado)
+VALUES (
+    id:INTEGERINSERT INTO posts (id, titulo, mensagem, autor, curtidas, fixado)
+    VALUES (
+        id:INTEGERINSERT INTO posts (id, titulo, mensagem, autor, curtidas, fixado)
+        VALUES (
+            id:INTEGER,
+            'titulo:TEXT',
+            'mensagem:TEXT',
+            'autor:TEXT',
+            curtidas:INTEGER,
+            fixado:INTEGER
+          );,
+        'titulo:TEXT',
+        'mensagem:TEXT',
+        'autor:TEXT',
+        curtidas:INTEGER,
+        fixado:INTEGER
+      );,
+    'titulo:TEXT',
+    'mensagem:TEXT',
+    'autor:TEXT',
+    curtidas:INTEGER,
+    fixado:INTEGER
+  );
