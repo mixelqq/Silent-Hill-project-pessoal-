0bbaa036fa918c1de53e390514918287520d5761
mox <miguel.fleck@rede.ulbra.br>
2026-07-30 19:50:04 -0300

    a

--- silenthill.session.sql
+++ silenthill.session.sql
@@ -1,25 +1,8 @@
-INSERT INTO posts (id, titulo, mensagem, autor, curtidas, fixado)
+INSERT INTO posts (titulo, mensagem, autor, curtidas, fixado)
 VALUES (
-    id:INTEGERINSERT INTO posts (id, titulo, mensagem, autor, curtidas, fixado)
-    VALUES (
-        id:INTEGERINSERT INTO posts (id, titulo, mensagem, autor, curtidas, fixado)
-        VALUES (
-            id:INTEGER,
-            'titulo:TEXT',
-            'mensagem:TEXT',
-            'autor:TEXT',
-            curtidas:INTEGER,
-            fixado:INTEGER
-          );,
-        'titulo:TEXT',
-        'mensagem:TEXT',
-        'autor:TEXT',
-        curtidas:INTEGER,
-        fixado:INTEGER
-      );,
-    'titulo:TEXT',
-    'mensagem:TEXT',
-    'autor:TEXT',
-    curtidas:INTEGER,
-    fixado:INTEGER
-  );
+    'Meu primeiro post',
+    'Bem-vindos ao fórum!',
+    'Miguel',
+    0,
+    0
+);
